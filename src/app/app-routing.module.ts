a88e4630187a669d1e24e292f9b74c2dcbf453d2
Elvis Rosa <you@example.com>
2023-01-09 20:57:29 -0500

    taller 1 de aplicaciones moviles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { IonicModule, IonicSlides } from '@ionic/angular';
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
   },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'action-shet',
+    loadChildren: () => import('./pages/action-shet/action-shet.module').then( m => m.ActionShetPageModule)
+  }
+  
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+    , IonicModule]
+  ,
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+
+interface Componente {
+  icon:string;
+  name:string;
+  redirecto:string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+
+export class InicioPage implements OnInit {
+
+  constructor() { }
+
+  componentes: Componente[]=[
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirecto: '/action-shet'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirecto: '/alert'
+    }
+  ]
+
+  ngOnInit() {
+  }
+
+}
